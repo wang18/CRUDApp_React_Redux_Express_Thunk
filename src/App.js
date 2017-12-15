f62dc2762474ee1efa2afc52fbfe1bfebbb23abb
wang18 <wang18@uoregon.edu>
2017-12-14 22:48:32 -0800

    add active class to top bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Link} from 'react-router-dom';
+import {Link, Route} from 'react-router-dom';
+
+const ActiveLink =({label, to, activeOnlyWhenExact}) => (
+    <Route path={to}
+           exact={activeOnlyWhenExact}
+           children={({match}) => (
+                <Link className={match ? 'active item' : 'item'}
+                      to={to}>{label}</Link>)}
+    />)
 
 class App extends Component {
   render() {
     return (
       <div className="ui container">
           <div className="ui three item menu">
-              <Link to="/" className="item">Home</Link>
-              <Link to="/games" className="item">Game</Link>
-              <Link to="/games/new" className="item">Add New Game</Link>
-
+              <ActiveLink activeOnlyWhenExact to="/" className="item" label="Home"/>
+              <ActiveLink activeOnlyWhenExact to="/games" className="item" label="Game"/>
+              <ActiveLink activeOnlyWhenExact to="/games/new" className="item" label="Add New Game"/>
           </div>
       </div>
     );
